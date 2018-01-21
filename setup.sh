6cc87207a93c938e2d22203f284923c9e6aded50
Davis Ford <ford_davis@bah.com>
2018-01-21 12:41:46 -0500

    finished setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-npm install -g truffle@3.4.11
+echo 'Starting installation...'
+echo '-----------'
+
+brew update
 brew tap ethereum/ethereum
-brew install ethereum
+brew install ethereum && brew upgrade ethereum
+npm install -g ethereumjs-testrpc && npm update -g ethereumjs-testrpc
+npm install -g truffle@3.4.11 # Older version to keep compatibility with course
+
+echo '-----------'
+echo 'Done'
